a83c334f4034896cff469b77aa165f006a8fdb6f
santanu <sharmakundan227@gmail.com>
2024-05-09 12:10:05 +0530

    output added to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        domains:["dzprrrwpybqxebloajfs.supabase.co"]
-    }
-}
+  images: {
+    domains: ["dzprrrwpybqxebloajfs.supabase.co"],
+  },
+  output: "export", // <=== enables static exports
+  reactStrictMode: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
