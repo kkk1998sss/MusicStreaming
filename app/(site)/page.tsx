2bc7a669524044e7f4c737ff6cb18274b356cb78
santanu <sharmakundan227@gmail.com>
2024-05-09 16:14:07 +0530

    bug fix

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -17,7 +17,7 @@ export default async function Home() {
             Welcome Back!
           </h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
-            <ListItem image="/images/liked.png" name="Liked Songs" href="liked"/>
+            <ListItem image="./images/liked.png" name="Liked Songs" href="liked"/>
           </div>
         </div>
       </Header>
